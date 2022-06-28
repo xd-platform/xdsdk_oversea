01aec1072e2d18e6dbce8f30a93019c8ee44e3b9
guiming <suguiming@xd.com>
2022-06-28 15:08:52 +0800

    refactor

---
+++ Plugins/iOS/Resource/Twitter/TDSGTwitterLoginKit.framework/Headers/TDSGlobalTwitterAuthToken.h
@@ -0,0 +1,19 @@
+//
+//  TDSGlobalTwitterAuthToken.h
+//  TDSGTwitterLoginKit
+//
+//  Created by JiangJiahao on 2021/4/23.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TDSGlobalTwitterAuthToken : NSObject
+
+@property (nonatomic,copy) NSString *key;
+@property (nonatomic,copy) NSString *secret;
+
+@end
+
+NS_ASSUME_NONNULL_END
